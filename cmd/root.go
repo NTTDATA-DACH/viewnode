d56372d55305bb7b9dc5223bd8fbbd9bbe9ef12b
Adam Boczek <adam.boczek@nttdata.com>
2021-07-14 15:48:30 +0200

    add namespace and all-namespaces flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,35 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+var namespace string
+var allNamespaces bool
 
 var rootCmd = &cobra.Command{
 	Use:   "kubectl-view-node",
 	Short: "kubectl-view-node shows nodes with their pods and containers.",
 	Long: `kubectl-view-node shows nodes with their pods and containers.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		lr := clientcmd.NewDefaultClientConfigLoadingRules()
+		if namespace == "" && !allNamespaces {
+			clientCfg, err := lr.Load()
+			if err != nil {
+				panic(err.Error())
+			}
+			namespace = clientCfg.Contexts[clientCfg.CurrentContext].Namespace
+			if namespace == "" {
+				namespace = "default"
+			}
+		}
+		if allNamespaces {
+			namespace = ""
+		}
+		fmt.Printf("namespace: %s\n", namespace)
+	},
 }
 
 func Execute() {
@@ -18,6 +40,9 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
+	rootCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "namespace to use")
+	rootCmd.Flags().BoolVarP(&allNamespaces, "all-namespaces", "A", false, "all namespaces to use")
 }
 
 func initConfig() {
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module kubectl-view-node
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/spf13/cobra v1.2.1
+	k8s.io/client-go v0.21.2
+)
