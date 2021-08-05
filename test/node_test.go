20c8c06980908c237d81cec4687c9bc45a18306b
Adam Boczek <adam.boczek@nttdata.com>
2021-08-05 14:43:41 +0200

    add mock implementation for retrieving pods

--- test/node_test.go
+++ test/node_test.go
@@ -10,12 +10,21 @@ type MockApi struct {}
 
 const NodesCount = 10
 const NodeName1 = "Node 1"
+const PodsCount = 3
+const PodName1 = "Pod 1"
 
 func (m MockApi) RetrieveNodeList() (*v1.NodeList, error) {
 	var nl v1.NodeList
 	nl.Items = make([]v1.Node, NodesCount)
 	nl.Items[0].Name = NodeName1
 	return &nl, nil
+}
+
+func (m MockApi) RetrievePodList(namespace string) (*v1.PodList, error) {
+	var pl v1.PodList
+	pl.Items = make([]v1.Pod, PodsCount)
+	pl.Items[0].Name = PodName1
+	return &pl, nil
 }
 
 func TestNodeTransform(t *testing.T) {
