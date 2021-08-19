f6334fa53219b3cc87decab715821c28a5d20438
Adam Boczek <adam.boczek@nttdata.com>
2021-08-19 09:30:57 +0200

    add info about number of scheduled and unscheduled pods

--- srv/view.go
+++ srv/view.go
@@ -34,16 +34,18 @@ func (vnd ViewNodeData) Printout() error {
 		fmt.Println("no nodes to display...")
 		return nil
 	}
-	up := len(vnd.Nodes[0].Pods)
-	fmt.Printf("%d unscheduled pod(s)", up)
-	if up > 0 {
+	nup := vnd.getNumberOfUnscheduledPods()
+	nsp := vnd.getNumberOfScheduledPods()
+	fmt.Printf("%d pod(s) in total\n", nup+nsp)
+	fmt.Printf("%d unscheduled pod(s)", nup)
+	if nup > 0 {
 		fmt.Printf(":")
 	}
 	fmt.Printf("\n")
 	for _, p := range vnd.Nodes[0].Pods {
 		fmt.Printf("  * %s (%s)\n", p.Name, p.Phase)
 	}
-	fmt.Printf("%d running node(s):\n", l-1)
+	fmt.Printf("%d running node(s) with %d scheduled pod(s):\n", l-1, nsp)
 	for _, n := range vnd.Nodes {
 		if n.Name != "" {
 			fmt.Printf("- %s running %d pod(s) (%s/%s)\n", n.Name, len(n.Pods), n.Os, n.Arch)
@@ -53,4 +55,19 @@ func (vnd ViewNodeData) Printout() error {
 		}
 	}
 	return nil
+}
+
+func (vnd ViewNodeData) getNumberOfUnscheduledPods() int {
+	if vnd.Nodes != nil {
+		return len(vnd.Nodes[0].Pods)
+	}
+	return 0
+}
+
+func (vnd ViewNodeData) getNumberOfScheduledPods() int {
+	var c int
+	for _, n := range vnd.Nodes[1:] {
+		c = c + len(n.Pods)
+	}
+	return c
 }
