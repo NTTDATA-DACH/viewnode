afb5b1417b972b7e8f94e4fe7e9b861a542f759d
Adam Boczek <adam.boczek@nttdata.com>
2022-03-01 14:25:58 +0100

    issue-5 add retrieving of metrics to the internal api

--- srv/api.go
+++ srv/api.go
@@ -2,6 +2,7 @@ package srv
 
 import (
 	"context"
+	"k8s.io/metrics/pkg/apis/metrics/v1beta1"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,6 +12,8 @@ import (
 type Api interface {
 	RetrieveNodeList() (*v1.NodeList, error)
 	RetrievePodList(namespace string) (*v1.PodList, error)
+	RetrieveNodeMetricses() (*v1beta1.NodeMetricsList, error)
+	RetrievePodMetricses(namespace string) (*v1beta1.PodMetricsList, error)
 }
 
 type KubernetesApi struct {
@@ -23,4 +26,12 @@ func (k KubernetesApi) RetrieveNodeList() (*v1.NodeList, error) {
 
 func (k KubernetesApi) RetrievePodList(namespace string) (*v1.PodList, error) {
 	return k.Setup.Clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+}
+
+func (k KubernetesApi) RetrieveNodeMetricses() (*v1beta1.NodeMetricsList, error) {
+	return k.Setup.MetricsClientset.MetricsV1beta1().NodeMetricses().List(context.TODO(), metav1.ListOptions{})
+}
+
+func (k KubernetesApi) RetrievePodMetricses(namespace string) (*v1beta1.PodMetricsList, error) {
+	return k.Setup.MetricsClientset.MetricsV1beta1().PodMetricses(namespace).List(context.TODO(), metav1.ListOptions{})
 }
