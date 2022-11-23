a39340589a0e9f97e4e31713f05ffae9185dfd81
Adam Boczek <adam.boczek@nttdata.com>
2022-11-23 03:42:22 +0100

    issue-8 add tests for error cases

--- srv/pod_test.go
+++ srv/pod_test.go
@@ -8,15 +8,17 @@ import (
 func TestPodFilter_LoadAndFilter(t *testing.T) {
 	var api MockApi
 	nf := NodeFilter{
-		Api: api,
+		Api:         api,
+		WithMetrics: true,
 	}
 	vns, err := nf.LoadAndFilter(nil)
 	assert.NilError(t, err)
 
 	pf := PodFilter{
-		Namespace:  "",
-		SearchText: PodName1,
-		Api:        api,
+		Namespace:   "",
+		SearchText:  PodName1,
+		Api:         api,
+		WithMetrics: false,
 	}
 	vns, err = pf.LoadAndFilter(vns)
 	assert.NilError(t, err)
@@ -24,4 +26,37 @@ func TestPodFilter_LoadAndFilter(t *testing.T) {
 	const expectedNoPods = 1
 	l := len(vns[1].Pods)
 	assert.Equal(t, expectedNoPods, l, "loading and filtering of pods was not correct; got: %d, expected %d pods", l, expectedNoPods)
+}
+
+func TestPodFilter_PodListError(t *testing.T) {
+	api := MockApi{
+		ApiTypeValue: PodListError,
+	}
+	nf := NodeFilter{
+		Api: api,
+	}
+	vns, err := nf.LoadAndFilter(nil)
+	assert.NilError(t, err)
+	pf := PodFilter{
+		Api: api,
+	}
+	_, err = pf.LoadAndFilter(vns)
+	assert.Error(t, err, "pod list error")
+}
+
+func TestPodFilter_PodMetricsesError(t *testing.T) {
+	api := MockApi{
+		ApiTypeValue: PodMetricsesError,
+	}
+	nf := NodeFilter{
+		Api: api,
+	}
+	vns, err := nf.LoadAndFilter(nil)
+	assert.NilError(t, err)
+	pf := PodFilter{
+		WithMetrics: true,
+		Api:         api,
+	}
+	_, err = pf.LoadAndFilter(vns)
+	assert.ErrorType(t, err, ErrMetricsServerNotInstalled)
 }
